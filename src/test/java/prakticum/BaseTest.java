576db426c94e647e82d8d12723ee05c137573162
Kseniia Trofimova <ksusha-300692@yandex.ru>
2025-07-16 22:09:20 +0700

    доработка третьего задания диплома

--- src/test/java/prakticum/BaseTest.java
+++ src/test/java/prakticum/BaseTest.java
@@ -17,7 +17,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public abstract class BaseTest {
+public class BaseTest {
     protected WebDriver driver;
     protected String browserName;
 
